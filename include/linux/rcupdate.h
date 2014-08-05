45761ee8d140960afd807055b833c93e8c452e50
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2014-08-05 05:23:35 -0700

    rcu: Remove redundant preempt_disable() from rcu_note_voluntary_context_switch()
    In theory, synchronize_sched() requires a read-side critical section
    to order against.  In practice, preemption can be thought of as
    being disabled across every machine instruction, at least for those
    machine instructions that are not in the idle loop and not on offline
    CPUs.  So this commit removes the redundant preempt_disable() from
    rcu_note_voluntary_context_switch().
    
    Please note that the single instruction in question is the store of
    zero to ->rcu_tasks_holdout.  The "if" is simply a performance optimization
    that avoids unnecessary stores.  To see this, keep in mind that both
    the "if" condition and the store are in a quiescent state.  Therefore,
    even if the task is preempted for a full grace period (presumably due
    to its having done a context switch beforehand), the store will be
    recording a legitimate quiescent state.
    
    Reported-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    
    Conflicts:
    	include/linux/rcupdate.h

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -297,10 +297,8 @@ extern void exit_rcu(void);
 extern struct srcu_struct tasks_rcu_exit_srcu;
 #define rcu_note_voluntary_context_switch(t) \
 	do { \
-		preempt_disable(); /* Exclude synchronize_sched(); */ \
 		if (ACCESS_ONCE((t)->rcu_tasks_holdout)) \
 			ACCESS_ONCE((t)->rcu_tasks_holdout) = false; \
-		preempt_enable(); \
 	} while (0)
 #else /* #ifdef CONFIG_TASKS_RCU */
 #define TASKS_RCU(x) do { } while (0)
